37085fa1388eb5d18d594dcaaca1b21df7f30f8a
adarshkunwar <hellboyadarsh18@gmail.com>
2022-06-27 18:20:01 +0545

    js complete

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-function jumpPlayer() {
-    let character = document.getElementById("player");
+function buttonClickUp() {
+   
+    let player = document.getElementsByClassName('player')[0];
+    player.classList.add('playerJump');
 
-    character.classList.add("jumpPlayer");
+    setTimeout(function() {
+        player.classList.remove('playerJump');
+    }, 500);
+    // player.classList.remove('playerDown');
 }
+
+setInterval(frame, 100)
+
+function frame() {
+    let player = document.getElementsByClassName('player')[0];
+    let enemy = document.getElementsByClassName('enemy')[0];
+
+    let playerValue = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+    console.log(playerValue);
+
+    let enemyValue = parseInt(window.getComputedStyle(enemy).getPropertyValue("left"));
+    // console.log(enemyValue);
+
+
+     if(enemyValue <150 && enemyValue > 50  && playerValue > 280) {
+        alert("you lose!");
+     }
+}
+
+
+
